a03acc9512ef2d5f8afcbc37c6519ab8c5df3867
Sonia Uppal <Sonia@calvisitor-10-105-132-38.calvisitor.1918.berkeley.edu>
2019-04-25 16:14:32 -0700

    basic structure for toxic tie

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,13 @@
-//document.getElementById('submitButton').onClickListener
+var submitButton = document.getElementById('submitButton');
+var phrases = document.getElementById('phrases');
+
+submitButton.onClick = function({
+  
+});
+
+let phraseArray = phrases.split(',');
+phrases.forEach(function(phrase){
+  phrase.trim();
+});
+
+let newPhraseText = document.createElement("p");
