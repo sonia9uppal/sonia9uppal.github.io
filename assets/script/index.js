f1e0c61aeab17c6be417b5932f7979ca90dcadf9
Sonia Uppal <Sonia@Sonias-MacBook-Pro-3.local>
2019-04-28 15:54:46 -0700

    links

---
+++ assets/script/index.js
@@ -0,0 +1,10 @@
+phraseButton = document.getElementById('phraseButton');
+bulletinButton = document.getElementById('bulletinButton');
+
+phraseButton.addEventListener('click', function() {
+  window.location.href="./form.html";
+});
+
+bulletinButton.addEventListener('click', function() {
+  window.location.href="./bulletin.html";
+});
