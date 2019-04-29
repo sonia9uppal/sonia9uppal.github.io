82c78d134df647b8e705a1bf5e9db0c772ba8c95
Sonia Uppal <Sonia@Sonias-MacBook-Pro-3.local>
2019-04-29 12:11:07 -0700

    phrase split

--- assets/script/form.js
+++ assets/script/form.js
@@ -1,16 +1,27 @@
 var textArea = document.getElementById('phrases');
 textArea.value="";
 textArea.focus();
+
 //textArea.select();
+var submitButton = document.getElementById('submitButton');
+var phrases;
+let phraseArray;
 
+/* Include date on phrase cards if time permits
 
-/*var submitButton = document.getElementById('submitButton');
-var phrases = textArea.value;
-console.log(phrases);
+var months = ['January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var year = curr.getFullYear()+'-'+(curr.getMonth()+1)+'-'+curr.getDate();
+var month = months[curr.getMonth()]; // String name of months
+var day = curr.getDate();
+*/
 
 submitButton.addEventListener('click', function() {
-  log('button clicked');
-});*/
+  phrases = document.getElementById('phrases').value;
+  phraseArray = phrases.split(',');
+  for (phrase of phraseArray) {
+    phrase.trim();
+  }
+});
 
 /*submitButton.onClick = function({
   console.log('button has been clicked');
