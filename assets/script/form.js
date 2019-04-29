e6fa0b9063bc27f690a407a9b202b8e76dcc911d
Sonia Uppal <Sonia@Sonias-MacBook-Pro-3.local>
2019-04-28 17:14:46 -0700

    page two styling don

--- assets/script/form.js
+++ assets/script/form.js
@@ -1,18 +1,24 @@
-var submitButton = document.getElementById('submitButton');
-var phrases = document.getElementById('phrases').value;
+var textArea = document.getElementById('phrases');
+textArea.value="";
+textArea.focus();
+//textArea.select();
+
+
+/*var submitButton = document.getElementById('submitButton');
+var phrases = textArea.value;
 console.log(phrases);
 
 submitButton.addEventListener('click', function() {
   log('button clicked');
-});
+});*/
 
 /*submitButton.onClick = function({
   console.log('button has been clicked');
 });*/
 
-let phraseArray = phrases.split(',');
+/*let phraseArray = phrases.split(',');
 for (phrase of phraseArray) {
   phrase.trim();
 }
 
-let newPhraseText = document.createElement("p");
+let newPhraseText = document.createElement("p");*/
