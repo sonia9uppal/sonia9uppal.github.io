652ea948ae8a8f0e692f9ec5d3bb6617ab20a044
Sonia Uppal <Sonia@Sonias-MacBook-Pro-3.local>
2019-04-30 13:05:11 -0700

    included endpoint to get

--- assets/script/bulletin.js
+++ assets/script/bulletin.js
@@ -1,5 +1,34 @@
 backButton = document.getElementById('backButton');
-
 backButton.addEventListener('click', function() {
   window.location.href="./form.html";
 });
+
+cardContainer = document.getElementById('card-container');
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://31ogaol5hd.execute-api.us-west-1.amazonaws.com/dev/phrases', true)
+request.onload = function() {
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status< 400) {
+    data.forEach(phrase => {
+      console.log(phrase);
+
+      var card = document.createElement('div');
+      card.setAttribute('class', 'fl tc pv4 card');
+
+      var h3 = document.createElement('h3');
+      h3.textContent = "\"" + phrase + "\"";
+
+      card.appendChild(h3);
+      cardContainer.appendChild(card);
+    });
+
+
+
+    // console.log(data);
+  } else {
+    console.log('The request status is ' + request.status);
+  }
+}
+
+request.send()
