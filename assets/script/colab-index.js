6870b24ba2d726f62840706e57791fc850d23411
Sonia Uppal <Sonia@Sonias-MacBook-Pro-8.local>
2019-10-26 16:00:53 -0700

    haha

---
+++ assets/script/colab-index.js
@@ -0,0 +1,10 @@
+phraseButton = document.getElementById('phraseButton');
+bulletinButton = document.getElementById('bulletinButton');
+
+phraseButton.addEventListener('click', function() {
+  window.location.href="./about.html";
+});
+
+bulletinButton.addEventListener('click', function() {
+  window.location.href="./dashboard.html";
+});
